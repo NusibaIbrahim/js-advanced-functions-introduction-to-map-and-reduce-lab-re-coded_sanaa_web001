a99d847572be7e01009c0bc8d021aec7eb65f1fd
Nusiba Ibrahim <ainusiba@gmail.com>
2020-09-02 07:02:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function mapToNegativize(arr) {
   arr.forEach((element) => newArr.push(element * -1));
   return newArr;
 }
+
+function mapToNoChange(arr) {
+  let newArr = [];
+  arr.forEach((element) => newArr.push(element));
+  return newArr;
+}
+
